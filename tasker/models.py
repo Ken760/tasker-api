b5eee729b06bf81e6c615d479641ecb98b043d5a
Ken760 <49761163+Ken760@users.noreply.github.com>
2021-12-08 10:59:52 +0300

    add base model for tasker

--- tasker/models.py
+++ tasker/models.py
@@ -1,3 +1,64 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+from django.conf import settings
+
+THEME = (
+    ("White", "White"),
+    ("Red", "Red"),
+    ("Green", "Green"),
+    ("Blue", "Blue"),
+    ("Yellow", "Yellow"),
+)
+
+
+class Task(models.Model):
+    """Задачи"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    title = models.CharField(max_length=40)
+    text = models.TextField(max_length=500, blank=True, null=True)
+    solution_text = models.TextField(max_length=500, blank=True, null=True)
+    url = models.SlugField(max_length=160, unique=True)
+    confirmed = models.BooleanField(default=False, name='isConfirmed')
+    created_date = models.DateTimeField(auto_now_add=True, name='createdDate')
+    updated_date = models.DateTimeField(auto_now=True, name='updatedDate')
+    theme = models.CharField(max_length=50, choices=THEME, default='White', null=True)
+    # rating = models.ForeignKey()
+    # comments = models.ForeignKey('Comment', blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+
+class Comment(models.Model):
+    """Отзывы"""
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField("Сообщение", max_length=1000)
+
+    def __str__(self):
+        return f"{self.text}"
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+
+
+class Rating(models.Model):
+    """Рейтинги"""
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    value = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self):
+        return f"{self.value}"
+
+    class Meta:
+        verbose_name = "Рейтинг"
+        verbose_name_plural = "Рейтинги"
