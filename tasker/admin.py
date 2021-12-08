cebcf8f6720278b93d336997fabf89439c04df23
Ken760 <49761163+Ken760@users.noreply.github.com>
2021-12-08 11:02:12 +0300

    adding models to the admin panel

--- tasker/admin.py
+++ tasker/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from accounts.models import UserAccount
+from tasker.models import *
 
 # Register your models here.
 admin.site.register(UserAccount)
+admin.site.register(Task)
+admin.site.register(Comment)
+admin.site.register(Rating)
