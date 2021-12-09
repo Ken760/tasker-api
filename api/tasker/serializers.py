d9d8f57a2fb420c1ac2d496594cbd3db16ec1ea9
Ken760 <49761163+Ken760@users.noreply.github.com>
2021-12-09 10:18:09 +0300

    Create serializers for tasker

---
+++ api/tasker/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from tasker.models import Task, Comment, Rating
+from rest_framework.pagination import PageNumberPagination, CursorPagination
+from rest_framework.relations import PrimaryKeyRelatedField
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор коментариев"""
+
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    """Сериализатор рейтинга"""
+
+    class Meta:
+        model = Rating
+        fields = "__all__"
+
+
+class TaskMainPageSerializer(serializers.ModelSerializer):
+    """Сериализатор задач"""
+    comments = CommentSerializer(many=True)
+    ratings = RatingSerializer(many=True)
+    createdDate = serializers.DateTimeField(format="%d %b", read_only=True)
+
+    class Meta:
+        model = Task
+        fields = "__all__"
