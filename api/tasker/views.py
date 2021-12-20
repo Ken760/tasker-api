0f98a00a5ae32072c57b4cd2787a92e2948b2d68
Ken760 <49761163+Ken760@users.noreply.github.com>
2021-12-20 18:25:57 +0300

    Update views.py

--- api/tasker/views.py
+++ api/tasker/views.py
@@ -76,7 +76,7 @@ class TaskPaginationView(generics.ListAPIView):
     serializer_class = TaskMainPageSerializer
     pagination_class = MyCursorPagination
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ('language', 'category', 'createdDate')
+    filterset_fields = ('language', 'category', 'createdDate', 'difficult')
     search_fields = ['language', 'difficult', 'category']
     ordering_fields = '__all__'
     # ordering = 'id'
