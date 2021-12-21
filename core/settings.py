31476d3b6be767e84dcf9b6fb580e6d5557ec394
Ken760 <49761163+Ken760@users.noreply.github.com>
2021-12-21 13:23:54 +0300

    static

--- core/settings.py
+++ core/settings.py
@@ -139,7 +139,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
