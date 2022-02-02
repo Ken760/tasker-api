9216aea18e121451f03c9700bfe6b93978b887d3
Ken760 <49761163+Ken760@users.noreply.github.com>
2022-02-02 19:32:55 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -34,9 +34,9 @@ ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ],
 }
 
