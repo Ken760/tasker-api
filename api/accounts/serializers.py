f6cf0f5dc6509c282708267d78a6d013705ad2e6
Ken760 <49761163+Ken760@users.noreply.github.com>
2022-02-10 21:13:32 +0300

    test

--- api/accounts/serializers.py
+++ api/accounts/serializers.py
@@ -5,7 +5,7 @@ from accounts.models import UserAccount
 User = get_user_model()
 
 
-class UserCreateSerializer(UserCreateSerializer):
+class UserCreateSerializer(WritableNestedModelSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = UserAccount
         fields = ('id', 'email', 'first_name', 'last_name', 'password', 'activity', 'nickname')
