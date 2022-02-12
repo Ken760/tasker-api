14d284c521cd2e961eb56a2e518ada7fb8fa0b86
Ken760 <49761163+Ken760@users.noreply.github.com>
2022-02-12 15:46:26 +0300

    fix

--- api/tasker/serializers.py
+++ api/tasker/serializers.py
@@ -37,7 +37,7 @@ class TaskCreateSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
     createdDate = serializers.DateTimeField(format="%d.%m.%Y", read_only=True)
     rating = serializers.FloatField(read_only=True)
-    userInfo = serializers.HiddenField(default=UserCreateSerializer())
+    userInfo = serializers.HiddenField(default=UserCreateSerializer)
 
     class Meta:
         model = Task
