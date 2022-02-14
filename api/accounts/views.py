0d3f9eab7b774847bd67d28cd7a771a63d6d31d5
Ken760 <49761163+Ken760@users.noreply.github.com>
2022-02-14 13:32:29 +0300

    adding fullname and minor fixes

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -5,13 +5,16 @@ from rest_framework.generics import (
 from rest_framework.permissions import IsAuthenticated
 from accounts.models import UserAccount
 from .permissions import IsOwnerProfileOrReadOnly
-from .serializers import UserProfileSerializer
+# from .serializers import UserProfileSerializer
 from django.contrib.auth import get_user_model
+
+from ..tasker.serializers import UserInfoSerializer
+
 User = get_user_model()
 
 
 class UserProfileDetailView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
-    serializer_class = UserProfileSerializer
+    serializer_class = UserInfoSerializer
     lookup_field = 'id'
     permission_classes = [IsOwnerProfileOrReadOnly,]
