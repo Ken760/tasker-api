cdb7ad79c6b7a056ea5bb60297e7767ed662c701
Ken760 <49761163+Ken760@users.noreply.github.com>
2022-02-15 18:41:03 +0300

    fix

--- tasker/models.py
+++ tasker/models.py
@@ -3,7 +3,7 @@ import uuid
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.conf import settings
-from mptt.fields import TreeForeignKey
+
 
 THEME = (
     ("white", "white"),
